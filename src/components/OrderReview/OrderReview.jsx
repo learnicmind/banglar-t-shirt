183697aa0fccff50221941b431df7e67eb97429c
Ariful Islam Razu <learnicmind@gmail.com>
2023-04-09 03:30:03 +0600

    first commit

---
+++ src/components/OrderReview/OrderReview.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const OrderReview = () => {
+    return (
+        <div>
+            <h2>this is order review page</h2>
+        </div>
+    );
+};
+
+export default OrderReview;
